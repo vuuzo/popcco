ffcc17381ae67ab2578c774f5e41cc3855ee7aeb
vuuzo <kaplann.oskar@gmail.com>
2026-01-26 03:57:50 +0100

    fix: commas

--- services/cloudinary.py
+++ services/cloudinary.py
@@ -4,8 +4,8 @@ import cloudinary.uploader
 
 class CloudinaryService:
     def __init__(self):
-        cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-        api_key=os.getenv("CLOUDINARY_API_KEY"),
+        cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME")
+        api_key=os.getenv("CLOUDINARY_API_KEY")
         api_secret=os.getenv("CLOUDINARY_API_SECRET")
 
         cloudinary.config(
